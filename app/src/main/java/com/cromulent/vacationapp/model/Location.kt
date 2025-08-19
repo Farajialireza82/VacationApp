ecc412082f26253a08a47da88c5ad02830bd0d3a
Farajialireza82 <farajialireza82@gmail.com>
2025-08-19 16:23:23 +0330

    updated Location data class

--- app/src/main/java/com/cromulent/vacationapp/model/Location.kt
+++ app/src/main/java/com/cromulent/vacationapp/model/Location.kt
@@ -8,10 +8,22 @@ data class Location(
     @SerializedName("name")
     val name: String,
     @SerializedName("distance")
-    val distance: String,
-    @SerializedName("bearing")
-    val bearing: String,
-    @SerializedName("address_obj")
-    val addressObj: Address,
+    val distance: String? = null,
+    @SerializedName("description")
+    val description: String? = null,
+    @SerializedName("num_reviews")
+    val reviewCount: String? = null,
+    @SerializedName("rating")
+    val rating: String? = null,
+    @SerializedName("latitude")
+    val latitude: String? = null,
+    @SerializedName("longitude")
+    val longitude: String? = null,
+    @SerializedName("amenities")
+    val amenities: List<String>? = null,
+    @SerializedName("see_all_photos")
+    val seeAllPhotosLink: String? = null,
+    @SerializedName("price_level")
+    val priceLevel: String? = null,
     var locationPhotos: List<LocationPhoto>? = null
 )
