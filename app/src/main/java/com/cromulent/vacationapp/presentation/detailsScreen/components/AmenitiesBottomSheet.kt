fd713e17e222c57025d62312561160ca4f345dcc
Farajialireza82 <farajialireza82@gmail.com>
2025-08-20 11:15:24 +0330

    improved amenities, description and show all button, empty states, reviews and ratings ui

---
+++ app/src/main/java/com/cromulent/vacationapp/presentation/detailsScreen/components/AmenitiesBottomSheet.kt
@@ -0,0 +1,75 @@
+package com.cromulent.vacationapp.presentation.detailsScreen.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.BottomSheetDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.cromulent.vacationapp.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AmenitiesBottomSheet(
+    amenities: List<String>,
+    onDismiss: () -> Unit
+) {
+    ModalBottomSheet(
+        containerColor = colorResource(R.color.white),
+        onDismissRequest = onDismiss,
+        dragHandle = { BottomSheetDefaults.DragHandle() }
+    ) {
+        Column(
+            modifier = Modifier.padding(
+                horizontal = 24.dp)
+        ) {
+            Text(
+                text = "All Facilities (${amenities.size})",
+                style = MaterialTheme.typography.headlineSmall,
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            LazyColumn {
+                items (amenities) { amenity ->
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 8.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            Icons.Default.Check,
+                            contentDescription = null,
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                        Spacer(modifier = Modifier.width(12.dp))
+                        Text(
+                            text = amenity,
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
